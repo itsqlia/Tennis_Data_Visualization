a89044bbef16280690d013b6554a45dbedbdf93d
Abigail O <75042354+itsqlia@users.noreply.github.com>
2021-06-05 22:51:30 +0200

    Matches + Duration, added Texts, finals.js

--- main.js
+++ main.js
@@ -1,65 +1,135 @@
 let stageHeight, stageWidth;
 let stage;
+let colors = {
+  "left": "rgba(249, 237, 102, 1)",
+  "right": "rgba(138, 227, 227, 1)",
+};
+const surfColors = {
+  "Hard": "rgba(121, 209, 209, 100)",
+  "Clay": "rgba(212, 95, 16, 100)",
+  "Carpet": "rgba(40, 86, 179, 100)",
+  "Grass": "rgba(171, 225, 132, 100)"
+};
 
 $(function () {
   stage = $('#stage');
   stageHeight = stage.height();
   stageWidth = stage.width();
-  drawDots();
+  prepareData();
+  drawMatches();
+  //drawDuration();
 });
 
-function drawDots() {
+function prepareData() {
+  
+  //F: Wie kann ich auf Monate sortieren lassen?
+  cumulateMonths = gmynd.cumulateData(data, "tourney_date");
+  //console.log(cumulateMonths);
 
-  data.forEach(player => {
-    if (player.winner_hand != player.loser_hand) {
-      let date = player.tourney_date;
-      let month = date.toString().charAt(4) + date.toString().charAt(5);
-      //console.log (month)
+  //Matches
+  groupedByWinner = gmynd.groupData(data, "winner_hand");
+  console.log ("Winner");
+  console.log (groupedByWinner);
 
-//for (let i=0; data2000.length; i++) {
+  //Surfaces
+  groupedBySurface = gmynd.groupData(data, "surface");
+  console.log("Surface");
+  console.log(groupedBySurface);
 
-      const circleGap = 20;
-      const r = 30;
-      let x = 100;
-      let y = 120;
+  //Final Winners
+  groupedByFinalWinners = gmynd.groupData(finals, "round");
+  console.log("Final Winners")
+  console.log(groupedByFinalWinners)
 
-      let dot = $('<div></div>');
-      dot.addClass("playerLeft");
-      dot.addClass("playerRight");
-      let color;
-      let stroke;
+};
 
-      if (month === "01") {
-        x = 100;
-        y = 120 + circleGap;
+function drawMatches() {
 
-        if (player.winner_hand === "L") {
-          color = 'playerLeft'
-        } else {
-          color = 'playerRight'
+  //for (i=0; i<data.length; i++){
+  data.forEach (player => {
+
+    //Fokus auf die Monate
+    let date = player.tourney_date;
+    let month = date.toString().charAt(4) + date.toString().charAt(5);
+    //console.log (month);
+    
+    
+    const circleGap = 20;
+        const r = 20;
+        let x = 150;
+        let y = 150;
+  
+        let dot = $('<div></div>');
+        dot.addClass("playerLeft");
+        dot.addClass("playerRight");
+        
+        //For-Schleife - Monate + Abfrage: Gewinner & Loser
+        
+        if (month === "01") {
+  
+          if (player.winner_hand === "L") {
+            color = colors.left
+          } else {
+            color = colors.right
+          }
+  
+          if (player.loser_hand === "L") {
+            stroke = 'playerLeft'
+          } else {
+            stroke = 'playerRight'
+          }
         }
-
-        if (player.loser_hand === "L") {
-          stroke = 'playerLeft'
-        } else {
-          stroke = 'playerRight'
-        }
+  
+        dot.css({
+          'height': r,
+          'width': r,
+          'background-color': 'white',
+          'position': 'absolute',
+          'left': x,
+          'top': y,
+          'border-radius': '100%',
+        });
+  
+        $('#stage').append(dot);
       }
+    
+    )
+  //}
+};
 
+function drawDuration() {
+  data.forEach(surface => {
+  const dotRadius = 10;
+  const radiusPerRing = 10;
+  const centerPoint = {
+    x : stageWidth / 2,
+    y : stageWidth / 2
+  }
 
-      dot.css({
-        'height': r,
-        'width': r,
-        'background-color': 'white',
-        'position': 'absolute',
-        'left': x,
-        'top': y,
-        'border-radius': '100%',
-      });
+  let dot = $('<div></div>');
+  dot.addClass("surface");
+  surfColors;
 
-      $('#stage').append(dot);
-    }
+  if (surface === "Hard") {
+    color = surfColors.Hard;
+  }
+  else if (surface === "Carpet") {
+    color = surfColors.Carpet;
+  }
+  else if (surface === "Clay") {
+    color = surfColors.Clay;
+  }
+  else if (surface === "Grass") {
+    color = surfColors.Grass;
+  }
+
+  dot.css({
+    'background-color': color,
+    'height': dotRadius * 2,
+    'width': dotRadius * 2,
+    'left': 50,
+    'top': 50,
   });
-}
-
-//};
+  $('#stage').append(dot);
+  })
+};
