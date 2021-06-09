b6f69796adb5c3de27edb7575824ceae9c83f439
Abigail O <75042354+itsqlia@users.noreply.github.com>
2021-06-09 11:24:39 +0200

    unnötige js.datas entfernt, tourneydate, data1.js

--- main.js
+++ main.js
@@ -1,142 +1,154 @@
 let stageHeight, stageWidth;
 let stage;
-const surfColors = {
-  "Hard": "rgba(121, 209, 209, 100)",
-  "Clay": "rgba(212, 95, 16, 100)",
-  "Carpet": "rgba(40, 86, 179, 100)",
-  "Grass": "rgba(171, 225, 132, 100)"
-};
+let matchesViewOn = true;
+let durationViewOn = false;
+let winnersViewOn = false;
+let groupedByDate;
 
 $(function () {
   stage = $('#stage');
   stageHeight = stage.height();
   stageWidth = stage.width();
   prepareData();
-  //drawMatches();
+  drawMatches();
   //drawDuration();
-  drawFinalWinners();
+  //drawFinalWinners();
+
+  if (matchesViewOn == true) {
+    matchesView();
+  }
+
 });
 
 function prepareData() {
-  
-  //F: Wie kann ich auf Monate sortieren lassen? 1 Spalte = 1 Monat
-  cumulateMonths = gmynd.cumulateData(data, "tourney_date");
-  //console.log(cumulateMonths);
 
-  //Matches
-  groupedByWinner = gmynd.groupData(data, "winner_hand");
+  data.forEach(match => {
+    let date = match.tourney_date;
+    let year = date.toString().charAt(0) + date.toString().charAt(1) + date.toString().charAt(2) + date.toString().charAt(3);
+    let month = date.toString().charAt(4) + date.toString().charAt(5);
+    //console.log(year, month);
+    match.tourneyYear = year;
+    match.tourneyMonth = month;
+  });
+
+  //Screen: Matches
+  groupedByDate = gmynd.groupData(data, ["year", "month"]);
   //console.log ("Winner");
   //console.log (groupedByWinner);
 
-  //Surfaces
+  //Screen: Surfaces
   groupedBySurface = gmynd.groupData(data, "surface");
   //console.log("Surface");
   //console.log(groupedBySurface);
 
-  //Final Winners
+  //Screen: Final Winners
   groupedByFinalWinners = gmynd.groupData(finalMatches, "year");
   console.log("Final Winners");
   console.log(groupedByFinalWinners);
-
 };
 
-function drawMatches() {
-
-  for (let key in groupedByWinner) {
-
-    let winnerHand = groupedByWinner[key];
+function drawMatches(year=2003) {
+  let yearData = groupedByDate[year.toString()];
+  for (let month in yearData) {
+    
+    let winnerHand = yearData[month];
     console.log("winnerHand");
     console.log(winnerHand);
 
-    winnerHand.forEach((player,j) => {
+    winnerHand.forEach((player, j) => {
 
-    //Fokus auf die Monate+Jahr
-    let date = player.tourney_date;
-    let year = date.toString().charAt(0) + date.toString().charAt(1) + date.toString().charAt(2) + date.toString().charAt(3);
-    let month = date.toString().charAt(4) + date.toString().charAt(5);
-    let tourneyDate = month.concat(year);
-    //console.log (tourneyDate);
+      //Fokus auf die Monate+Jahr
 
-        const r = 12;
-        const circleGap = r;
-        const blockGap = 3 * r;
+      //let tourneyDate = month.concat(year);
+      //console.log (tourneyDate);
 
-        const columnNumber = j % 12;
-        let blockOffSet = columnNumber >= 12 ? blockGap : 0;
-        const x = (3 + columnNumber * r) + blockOffSet;
-        const lineNumber = Math.floor (j/10);
-        const blockNumber = Math.floor (lineNumber/5);
-        const y = (lineNumber * 3 * r) + (blockNumber * blockGap);
-  
-        let dot = $('<div></div>');
-        dot.addClass("finalWinners");
-        let border;
-        
-        let color = 'rgba(249, 237, 102, 1)';
+      const r = 12;
+      const circleGap = 5 * r;
+      const blockGap = 2 * r;
 
-        
-        //For-Schleife - Monate + Abfrage: Gewinner & Loser
-    
-          if (player.winner_hand === "L") {
-            color = 'rgba(249, 237, 102, 1)'
-          }
-        
-  
-        dot.css({
-          'height': r * 2,
-          'width': r * 2,
-          'background-color': 'white',
-          'position': 'absolute',
-          'left': x,
-          'top': y,
-          'border-radius': '100%',
-        });
-  
-        $('#stage').append(dot);
+      const columnNumber = j % 2;
+      let blockOffSet = columnNumber >= 12 ? blockGap : 0;
+      const x = (3 + columnNumber * r) + blockOffSet;
+      const lineNumber = Math.floor(j / 10);
+      const blockNumber = Math.floor(lineNumber / 2);
+      const y = 1.5 * (blockNumber * blockGap);
+
+      let dot = $('<div></div>');
+      dot.addClass("finalWinners");
+      let border;
+
+      let color = 'rgba(249, 237, 102, 1)';
+
+
+      //For-Schleife - Monate + Abfrage: Gewinner & Loser
+
+      if (player.winner_hand === "L" & year === "2003") {
+        console.log(player.winner_hand);
+
+        color = 'rgba(249, 237, 102, 1)'
       }
-    
+      else if (player.loser_hand === "L" & year === "2003") {
+        console.log(player.loser_hand);
+
+        border = '2px solid' + 'rgba(249, 237, 102, 0.5)'
+      }
+
+
+      dot.css({
+        'height': r * 2,
+        'width': r * 2,
+        'background-color': color.finalWinners,
+        'position': 'absolute',
+        'left': x,
+        'top': y,
+        'border-radius': '100%',
+      });
+
+      $('#stage').append(dot);
+    }
+
     )
   }
 };
 
 function drawDuration() {
   data.forEach(surface => {
-  const dotRadius = 10;
-  const radiusPerRing = 10;
-  const centerPoint = {
-    x : stageWidth / 2,
-    y : stageWidth / 2
-  }
+    const dotRadius = 10;
+    const radiusPerRing = 10;
+    const centerPoint = {
+      x: stageWidth / 2,
+      y: stageWidth / 2
+    }
 
-  let dot = $('<div></div>');
-  dot.addClass("surface");
-  surfColors;
+    let dot = $('<div></div>');
+    dot.addClass("surface");
+    surfColors;
 
-  if (surface === "Hard") {
-    color = surfColors.Hard;
-  }
-  else if (surface === "Carpet") {
-    color = surfColors.Carpet;
-  }
-  else if (surface === "Clay") {
-    color = surfColors.Clay;
-  }
-  else if (surface === "Grass") {
-    color = surfColors.Grass;
-  }
+    if (surface === "Hard") {
+      color = surfColors.Hard;
+    }
+    else if (surface === "Carpet") {
+      color = surfColors.Carpet;
+    }
+    else if (surface === "Clay") {
+      color = surfColors.Clay;
+    }
+    else if (surface === "Grass") {
+      color = surfColors.Grass;
+    }
 
-  dot.css({
-    'background-color': color,
-    'height': dotRadius * 2,
-    'width': dotRadius * 2,
-    'left': 50,
-    'top': 50,
-  });
-  $('#stage').append(dot);
+    dot.css({
+      'background-color': color,
+      'height': dotRadius * 2,
+      'width': dotRadius * 2,
+      'left': 50,
+      'top': 50,
+    });
+    $('#stage').append(dot);
   })
 };
 
-function drawFinalWinners(){
+function drawFinalWinners() {
 
   let i = 0;
 
@@ -146,12 +158,12 @@ function drawFinalWinners(){
 
     finalWinners.forEach((player, j) => {
 
-      const rWin = 10;
+      const rWin = 9;
 
       let theta = 2.4 * j;
-      let spiralRadius = 6 * Math.sqrt(theta) * 2;
-      let xWin = 160 + Math.cos(theta) * spiralRadius + (i * 200);
-      let yOffset = i % 2 * 390;
+      let spiralRadius = 5 * Math.sqrt(theta) * 2.4;
+      let xWin = 160 + Math.cos(theta) * spiralRadius + (i * 190);
+      let yOffset = i % 3 * 260; //0-5 bis 6-11
       let yWin = 320 + Math.sin(theta) * spiralRadius + yOffset;
 
       let dot = $('<div></div>');
@@ -175,3 +187,48 @@ function drawFinalWinners(){
     i++;
   }
 };
+
+//Switch screens
+
+function matchesView() {
+  matchesViewOn = true;
+  durationViewOn = false;
+  winnersViewOn = false;
+
+  drawMatches();
+
+  $('.matches').css({
+    'color': "white",
+  });
+
+  $('.duration').css({
+    'color': "rgba(255, 255, 255, 0.5)",
+  });
+
+  $('.winners').css({
+    'color': "rgba(255, 255, 255, 0.5)",
+  });
+
+}
+
+function winnersView() {
+  matchesViewOn = false;
+  durationViewOn = false;
+  winnersViewOn = true;
+
+  drawFinalWinners();
+
+  $('.matches').css({
+    'color': "rgba(255, 255, 255, 0.5)",
+  });
+
+  $('.duration').css({
+    'color': "rgba(255, 255, 255, 0.5)",
+  });
+
+  $('.winners').css({
+    'color': "white",
+  });
+
+  $('.inner').fadeIn();
+}
